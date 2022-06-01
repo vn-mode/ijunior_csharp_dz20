1d8ac7c0c66fbef88f4fd533127fd2dbf1028f78
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-06-01 16:13:56 +0300

    dz20
    dz20

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[3, 3];
+            int secondRowSum = 0;
+            int productOfNumbersColumn = 1;
+            Random random = new Random();
+            Console.WriteLine("Исходная матрица:\n");
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = random.Next(1, 9);
+                    Console.Write(myArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for(int i = 0; i < myArray.GetLength(1); i++)
+            {
+                secondRowSum += myArray[1, i];
+            }
+            Console.WriteLine($"\nСумма второй строки = {secondRowSum}");
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                productOfNumbersColumn *= myArray[i, 0];
+            }
+            Console.WriteLine($"Произведение чисел первого столбца = {productOfNumbersColumn}");
+        }
+    }
+}
