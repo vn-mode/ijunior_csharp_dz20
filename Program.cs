dba1368c0c6ca6de76f71bc747da1445c804b5d8
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-06-01 16:22:08 +0300

    dz20
    dz20

--- Program.cs
+++ Program.cs
@@ -6,29 +6,31 @@ namespace vn_mode_csharp_dz20
     {
         static void Main(string[] args)
         {
-            int[,] myArray = new int[3, 3];
+            int[,] matrix = new int[3, 3];
             int secondRowSum = 0;
             int productOfNumbersColumn = 1;
+            int minNumber = 1;
+            int maxNumber = 9;
             Random random = new Random();
             Console.WriteLine("Исходная матрица:\n");
 
-            for (int i = 0; i < myArray.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < myArray.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    myArray[i, j] = random.Next(1, 9);
-                    Console.Write(myArray[i, j] + " ");
+                    matrix[i, j] = random.Next(minNumber, maxNumber);
+                    Console.Write(matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-            for(int i = 0; i < myArray.GetLength(1); i++)
+            for(int i = 0; i < matrix.GetLength(1); i++)
             {
-                secondRowSum += myArray[1, i];
+                secondRowSum += matrix[1, i];
             }
             Console.WriteLine($"\nСумма второй строки = {secondRowSum}");
-            for (int i = 0; i < myArray.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                productOfNumbersColumn *= myArray[i, 0];
+                productOfNumbersColumn *= matrix[i, 0];
             }
             Console.WriteLine($"Произведение чисел первого столбца = {productOfNumbersColumn}");
         }
